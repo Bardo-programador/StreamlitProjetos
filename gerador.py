d0526463115e2b91cd28f2440a77213eafb47466
Samuel Roberto <samucadeus23@gmail.com>
2023-05-15 20:33:25 -0300

    Home e o gerador de senhas

---
+++ gerador.py
@@ -0,0 +1,15 @@
+from random import choice
+
+# numero_digitos = int(input("quantos caracteres a senha deve ter?"))
+letras_maiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+letras_minusculas = "abcdefghijklmnopqrstuvwxyz"
+numeros = "1234567890"
+especiais ="!@#$%&*-+/_"
+opcoes = [letras_maiusculas, letras_maiusculas, numeros, especiais]
+def gerador_de_senha(numero_caracteres, caracteres):
+    senha = ""
+    for i in range(numero_caracteres):
+        senha += choice(caracteres)
+    return senha
+
+
---
+++ main.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import gerador
+
+caixa_seletora = st.sidebar.selectbox("Quis projetos deseja navegar:",
+                                      ("Home","Gerador de senha",))
+
+if caixa_seletora == "Home":
+    st.title("Página inicial")
+
+if caixa_seletora == "Gerador de senha":
+    caracteres = ""
+    st.title("Geradora de senhas")
+    with st.sidebar:
+        maiusculos = st.checkbox("Letras maiusculas")
+        minusculos = st.checkbox("Letras minusculas")
+        numeros = st.checkbox("Números")
+        especiais = st.checkbox("Especias")
+        opcoes = [maiusculos, minusculos, numeros, especiais]
+        slider = st.slider("Quantos números", 1, 128, 8)
+        botao = st.button('Gerar Senha')
+
+    if botao:
+        try:
+            for opcao, escolha in zip(gerador.opcoes, opcoes):
+                if escolha:
+                    caracteres += opcao
+            senha = gerador.gerador_de_senha(slider, caracteres)
+            caracteres = ""
+            st.write(f"A senha é:\n{senha}")
+        except:
+            st.write("Escolha uma opcao")
+
+
+
+
+
+
+
