5bff4a1141e59f90212740894cb48dedaf4e3769
Samuel Roberto <samucadeus23@gmail.com>
2023-05-15 21:21:39 -0300

    correção no gerador

--- gerador.py
+++ gerador.py
@@ -5,7 +5,7 @@ letras_maiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 letras_minusculas = "abcdefghijklmnopqrstuvwxyz"
 numeros = "1234567890"
 especiais ="!@#$%&*-+/_"
-opcoes = [letras_maiusculas, letras_maiusculas, numeros, especiais]
+opcoes = [letras_maiusculas, letras_minusculas, numeros, especiais]
 def gerador_de_senha(numero_caracteres, caracteres):
     senha = ""
     for i in range(numero_caracteres):
