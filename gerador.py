5add45d192faf7728593d531d88740712dee26c2
Samuel Roberto <samucadeus23@gmail.com>
2023-07-11 12:05:08 -0300

    Adicionado algoritmo de a lista de opcoes ifra cesar

--- gerador.py
+++ gerador.py
@@ -4,12 +4,12 @@ from random import choice
 letras_maiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 letras_minusculas = "abcdefghijklmnopqrstuvwxyz"
 numeros = "1234567890"
-especiais ="!@#$%&*-+/_"
+especiais ="!@#%&-+/_"
 opcoes = [letras_maiusculas, letras_minusculas, numeros, especiais]
 def gerador_de_senha(numero_caracteres, caracteres):
     senha = ""
     for i in range(numero_caracteres):
-        senha += choice(caracteres)
+        senha += choice(caracteres) 
     return senha
 
 
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import gerador
+import cifra_cesar
 
 caixa_seletora = st.sidebar.selectbox("Quis projetos deseja navegar:",
-                                      ("Home","Gerador de senha",))
+                                      ("Home","Gerador de senha","Cifra Cesar"))
 
 if caixa_seletora == "Home":
     st.title("Página inicial")
@@ -25,12 +26,26 @@ if caixa_seletora == "Gerador de senha":
                 if escolha:
                     caracteres += opcao
             senha = gerador.gerador_de_senha(slider, caracteres)
-            caracteres = ""
             st.write(f"A senha é:\n{senha}")
         except:
             st.write("Escolha uma opcao")
 
+if caixa_seletora == "Cifra Cesar":
+    st.title("Cifra de Cesar")
+    with st.sidebar:
+        texto = st.text_area("Digite o texto")
+        chave = st.number_input("Digite a chave", 1, 26, 3)
+        cifrar = st.button("Cifrar")
+        decifrar = st.button("Decifrar")
 
+    if cifrar:
+        texto = str(texto)
+        st.title("Mensagem cifrada")
+        st.write(cifra_cesar.cesar(texto, chave, 1))
+    if decifrar:
+        texto = str(texto)
+        st.title("Mensagem decifrada")
+        st.write(cifra_cesar.cesar(texto, chave, -1))
 
 
 
